58544b0ebdb340d5ad7a3a4bddc3a1e3e0be2525
hmart424 <95835840+hmart424@users.noreply.github.com>
2022-05-15 11:22:37 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-connection = psycopg2.connect
-    host = 'nba-hof-project.cxpeww6dbftb.us-east-2.rds.amazonaws.com',
-    port = 5432,
-    user = 'postgres',
-    password = 'tC1sfkNBaUqSH4noRcz6',
-    database='postgres
-    
